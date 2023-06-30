fdbad7366825f728df1396a947588fd2280ca8aa
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2023-06-30 19:31:14 +0200

    Remove resume stuff and simplified site (#2)

--- build.jsx
+++ build.jsx
@@ -2,12 +2,10 @@ const fs = require("node:fs/promises");
 const path = require("node:path");
 const React = require("react");
 const {renderToStaticMarkup} = require("react-dom/server");
-const runtime = require("react/jsx-runtime");
-const matter = require("gray-matter");
 
 // Import data
-const pkg = require("./package.json");
 const icons = require("./icons.json");
+const data = require("./data.json");
 
 // Generate build info
 const getBuildInfo = () => {
@@ -27,35 +25,24 @@ const getBuildInfo = () => {
     };
 };
 
-const readMarkdownFile = (folder, file) => {
-    const filePath = path.join(folder, file);
-    return fs.readFile(filePath, "utf8").then(fileContent => {
-        const {data, content} = matter(fileContent);
-        return {
-            data: data,
-            content: content,
-            fileName: path.basename(file, ".mdx") + ".html",
-        };
-    });
-};
+const Icon = props => (
+    <svg xmlns="http-//www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+        <path d={icons[props.icon]} fill="none" strokeWidth="2" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+);
 
-// Fetch imports before running build script
-const fetchImports = () => {
-    return Promise.all([
-        import("@mdx-js/mdx"),
-    ]);
-};
+const PrimaryButton = props => (
+    <a href={props.to} className="flex items-center justify-center gap-2 rounded-md px-6 py-4 text-white bg-gray-800 hover:bg-gray-600 no-underline">
+        {props.icon && (
+            <div className="flex items-center text-2xl">
+                <Icon icon={props.icon} />
+            </div>
+        )}
+        <div className="font-bold text-lg">{props.text}</div>
+    </a>
+);
 
-const pageComponents = {
-    Icon: props => (
-        <svg xmlns="http-//www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
-            <path d={icons[props.icon]} fill="none" strokeWidth="2" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" />
-        </svg>
-    ),
-    Fragment: React.Fragment,
-};
-
-const PageWrapper = props => (
+const Page = props => (
     <html lang="en">
         <head>
             <meta charSet="utf-8" />
@@ -69,114 +56,92 @@ const PageWrapper = props => (
             <meta property="og:image" content="https://www.josemi.xyz/og.png" />
             <meta property="og:description" content="Hey! I'm Josemi, a mathematician, frontend developer and product designer." />
             <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700;800;900&display=swap" />
-            <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Crimson+Pro:wght@700;900&display=swap" />
             <link rel="stylesheet" href={"./low.css"} />
             <link rel="stylesheet" href={"./resume.css"} />
-            {/* <title>{props.page.data.title}</title> */}
             <title>Josemi Juanes, Ph.D.</title>
         </head>
-        <body className="bg-white m-0 p-0 font-inter text-gray-700 leading-normal">
-            {props.page.data?.layout === "default" && (
-                <React.Fragment>
-                    {/* Header content */}
-                    <div className="border-b-1 border-gray-300 relative">
-                        <div className="w-full maxw-7xl h-20 px-6 mx-auto flex items-center justify-between">
-                            <a href="./index.html" className="flex items-center gap-1 text-gray-800 no-underline">
-                                <div className="flex items-center h-16">
-                                    <img src="avatar.png" className="h-full" />
-                                </div>
-                                <div className="font-black font-crimson text-2xl tracking-tight">
-                                    <span>josemi.</span>
-                                </div>
-                            </a>
-                            <div className="hidden sm:flex gap-6 items-center">
-                                {props.pages.filter(p => p.fileName !== "index.html").map(page => (
-                                    <a key={page.fileName} href={page.fileName} className="no-underline text-gray-800 hover:text-gray-600">
-                                        <span className="font-medium">{page.data.title}</span>
-                                    </a>
-                                ))}
-                            </div>
-                        </div>
+        <body className="bg-white m-0 p-0 font-inter text-gray-800 leading-normal">
+            {/* Main content */}
+            <div className="w-full maxw-7xl mx-auto px-6">
+                <div className="flex flex-col py-32">
+                    <div className="text-6xl md:text-7xl text-gray-800 font-black leading-tight">
+                        <span>Hi! I'm Josemi.</span>
                     </div>
-                    {/* Main content */}
-                    <div className="w-full maxw-7xl mx-auto px-6">
-                        {props.pageContent}
+                    <div className="w-full maxw-4xl mt-4 text-2xl text-gray-700">
+                        <span>I am a passionate <b>mathematician</b> turned <b>React</b> developer. I combine my mathematical background with my love for design to craft beautiful and performant user interfaces.</span>
                     </div>
-                    {/* Contact and footer section */}
-                    <div className="w-full maxw-7xl mx-auto px-6 pt-24 pb-32">
-                        <div className="font-crimson font-black text-4xl sm:text-6xl mb-6 tracking-tight leading-tight">
-                            <a href={`mailto:${pkg.author.email}`} className="no-underline text-gray-800 hover:text-gray-600">
-                                <span>{pkg.author.email}</span>
-                            </a>
-                        </div>
-                        <div className="w-full maxw-3xl leading-relaxed text-lg mb-8">
-                            Thank you for visiting my online profile! 
-                            I look forward to the opportunity to contribute my skills and expertise to your next project.
-                        </div>
-                        <div className="flex gap-4 items-center mb-20">
-                            {Object.keys(pkg.social).map(key => (
-                                <a key={key} href={pkg.social[key]} target="_blank" className="no-underline o-100 hover:o-70">
-                                    <img className="w-8 h-8" src={`./${key}.svg`} />
+                    <div className="mt-10 md:flex">
+                        <PrimaryButton text="Resume" icon="file" to="https://resume.josemi.xyz" />
+                    </div>
+                </div>
+                <div className="sm:py-24 py-16">
+                    <div className="font-black text-4xl sm:text-6xl text-gray-800 mb-8 sm:mb-12 leading-tight">
+                        <span>Projects</span>
+                    </div>
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                        {props.data.projects.map(project => (
+                            <div key={project.title} className="flex flex-col p-10 bg-gray-100 rounded-xl">
+                                <a href={project.url} target="_blank" className="no-underline hover:underline text-gray-800">
+                                    <div className="font-bold text-gray-800 text-3xl leading-none">
+                                        <span>{project.title}</span>
+                                    </div>
                                 </a>
-                            ))}
-                        </div>
-                        <div className="text-sm">
-                            <div className="mb-2">Hand-crafted with care and love by <b>Josemi</b>.</div>
-                            <div>Last built on {props.buildInfo.time.formatted}.</div>
-                        </div>
+                                <div className="mt-4">{project.description}</div>
+                                {project.keywords.length > 0 && (
+                                    <div className="mt-6 flex gap-2 flex-wrap">
+                                        {project.keywords.map(key => (
+                                            <div key={key} className="rounded-full px-3 py-1 bg-gray-300 text-xs text-gray-800">
+                                                <span>{key}</span>
+                                            </div>
+                                        ))}
+                                    </div>
+                                )}
+                            </div>
+                        ))}
                     </div>
-                </React.Fragment>
-            )}
-            {props.page.data?.layout === "empty" && (
-                <React.Fragment>
-                    {props.pageContent}
-                </React.Fragment>
-            )}
+                </div>
+            </div>
+            {/* Contact and footer section */}
+            <div className="w-full maxw-7xl mx-auto px-6 pt-24 pb-32">
+                <div className="font-black text-4xl sm:text-6xl mb-6 leading-tight">
+                    <a href={`mailto:${props.data.email}`} className="no-underline hover:underline text-gray-800 hover:text-gray-700">
+                        <span>{props.data.email}</span>
+                    </a>
+                </div>
+                <div className="w-full maxw-3xl leading-relaxed text-lg mb-8">
+                    Thank you for visiting my online profile! 
+                    I look forward to the opportunity to contribute my skills and expertise to your next project.
+                </div>
+                <div className="flex gap-4 items-center mb-20">
+                    {props.data.social.map(item => (
+                        <a key={item.url} href={item.url} target="_blank" className="no-underline o-100 hover:o-70">
+                            <img className="w-8 h-8" src={item.image} />
+                        </a>
+                    ))}
+                </div>
+                <div className="text-sm">
+                    <div className="mb-2">Hand-crafted with care and love by <b>Josemi</b>.</div>
+                    <div>Last built on {props.buildInfo.time.formatted}.</div>
+                </div>
+            </div>
         </body>
     </html>
 );
 
-fetchImports().then(imports => {
-    const [mdx] = imports;
-    const log = msg => console.log(`[build] ${msg}`);
+const main = () => {
     const buildInfo = getBuildInfo();
-    const inputFolder = path.join(process.cwd(), "pages");
-    const outputFolder = path.join(process.cwd(), "public");
-    log("Starting build...");
-    log("Reading .mdx files from 'pages/' folder.");
-    fs.readdir(inputFolder)
-        .then(files => files.filter(file => path.extname(file) === ".mdx"))
-        .then(files => {
-            log(`Processing ${files.length} files.`);
-            return Promise.all(files.map(file => {
-                return readMarkdownFile(inputFolder, file);
-            }));
+    return Promise.resolve(true)
+        .then(() => {
+            const pageContent = React.createElement(Page, {
+                data: data,
+                buildInfo: buildInfo,
+            });
+            return renderToStaticMarkup(pageContent);
         })
-        .then(pages => {
-            return Promise.all(pages.map(page => {
-                return mdx.evaluate(page.content, {...runtime})
-                    .then(pageComponent => {
-                        const pageContent = React.createElement(PageWrapper, {
-                            pageContent: React.createElement(pageComponent.default, {
-                                page: page,
-                                components: pageComponents,
-                                data: {
-                                    version: pkg.version,
-                                    author: pkg.author,
-                                    social: pkg.social,
-                                },
-                            }),
-                            page: page,
-                            pages: pages,
-                            buildInfo: buildInfo,
-                        });
-                        return renderToStaticMarkup(pageContent);
-                    })
-                    .then(content => fs.writeFile(path.join(outputFolder, page.fileName), content, "utf8"))
-                    .then(() => {
-                        log(`Saved file 'public/${page.fileName}'.`);
-                    });
-            }));
-        })
-        .then(() => log("Build finished."));
-});
+        .then(content => fs.writeFile(path.join(process.cwd(), "public", "index.html"), content, "utf8"))
+        .then(() => {
+            console.log("Build finished.");
+        });
+};
+
+main();
