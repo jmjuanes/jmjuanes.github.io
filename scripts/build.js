9fe20f6662db1756402db1cf5c9348db03337088
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2024-08-28 16:13:36 +0000

    Parse posts

--- scripts/build.js
+++ scripts/build.js
@@ -1,5 +1,6 @@
 import * as fs from "node:fs";
 import * as path from "node:path";
+import * as marked from "marked";
 import mikel from "mikel";
 
 // globals
@@ -49,6 +50,23 @@ const getData = () => {
     return Object.fromEntries(dataEntries);
 };
 
+// get posts
+const getPosts = () => {
+    const postsFolder = path.join(process.cwd(), "posts");
+    return fs.readdirSync(postsFolder, "utf8")
+        .filter(file => path.extname(file) === ".md")
+        .map(file => path.join(postsFolder, file))
+        .map(file => {
+            const content = mikel.frontmatter(fs.readFileSync(file, "utf8"));
+            return {
+                name: path.basename(file, ".md"),
+                url: path.join("/", path.basename(file, ".md") + ".html"),
+                data: content.data,
+                content: marked.parse(content.body),
+            };
+        });
+};
+
 // global data object
 const globalData = {
     site: {
@@ -59,7 +77,7 @@ const globalData = {
             links: [
                 {text: "about", href: "/"},
                 {text: "projects", href: "/projects"},
-                {text: "writtings", href: "/writtings"},
+                {text: "notes", href: "/notes"},
                 // {text: "resume", href: "/resume"},
             ],
         },
@@ -73,12 +91,12 @@ const globalData = {
     },
     page: null,
     pages: getPages(),
-    posts: [],
+    posts: getPosts(),
     data: getData(),
 };
 
-// build pages
-globalData.pages.forEach(page => {
+// build stuff
+[...globalData.pages, ...globalData.posts].forEach(page => {
     console.log(`[build] save ${page.url}`);
     globalData.page = page; // set current page in global data object
     globalData.page.content = mikel(page.content, globalData); // compile page content
