5c05f252ac93b79789344731f0fa6eb54cf4bac4
Josemi <josemijuanes@gmail.com>
2026-02-11 20:40:11 +0100

    Initialize branding package

---
+++ packages/brand/scripts/build.ts
@@ -0,0 +1,38 @@
+import fs from "node:fs/promises";
+import postcss from "postcss";
+import postcssImport from "postcss-import";
+import autoprefixer from "autoprefixer";
+import { minify } from "csso";
+import lowcss from "lowcss";
+
+const INPUT_FILE = "index.css";
+const OUTPUT_FILE = "brand.css";
+
+const build = (): Promise<void> => {
+    return fs.readFile(INPUT_FILE, "utf8")
+        .then(input => {
+            const plugins: any[] = [
+                autoprefixer,
+                postcssImport,
+                lowcss,
+            ];
+            return postcss(plugins).process(input, {
+                from: INPUT_FILE,
+            });
+        })
+        .then(result => {
+            return result;
+            // print all warnings (if any)
+            result.warnings().forEach(warn => {
+                console.warn(warn.toString());
+            });
+            return minify(result.css, {
+                sourceMap: false,
+            });
+        })
+        .then(result => {
+            return fs.writeFile(OUTPUT_FILE, result.css);
+        });
+};
+
+build().catch(console.error);
