238be68fb692681179a1646e728ee144eae9e6c7
Josemi Juanes <5751201+jmjuanes@users.noreply.github.com>
2026-02-15 21:36:06 +0100

    Initialize plugin for josemi-brand

---
+++ packages/brand/plugin.ts
@@ -0,0 +1,26 @@
+import fs from "node:fs";
+import path from "node:path";
+import type { MikelOptions, MikelPartial } from "mikel";
+
+// export josemi-brand plugin for mikel engine
+export default (options: any = {}): Partial<MikelOptions> => {
+    // 1. read components and layouts and save them as partials
+    const partials = {} as Record<string, string>;
+    ["components", "layouts"].forEach(folder => {
+        const folderPath = path.resolve(folder);
+        fs.readdirSync(folderPath, "utf8").forEach(file => {
+            if (path.extname(file) === ".mustache") {
+                partials[file] = fs.readFileSync(path.join(folderPath, file), "utf8");
+            }
+        });
+    });
+    // 2. return the new options for mikel
+    return {
+        partials: partials,
+        variables: {
+            josemi: {
+                iconsPath: options?.iconsPath || "/icons.svg",
+            },
+        },
+    };
+};
